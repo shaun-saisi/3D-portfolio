96585db21a4747dca3a08c2a6df627f23ebd6d4c
pwaguzi <saisishaun4@gmail.com>
2024-03-11 20:51:37 +0300

    completed the design of the projects page

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -44,24 +44,21 @@ image, source_code_link } ) => {
             />
 
           </div>
-
-          <div 
-            onClick={() => window.open 
-            (source_code_link, "blank")}
-            className='black-gradient w-10 h-10 rounded-full 
-            flex justify-center cursor-pointer'
-          >
-            <img 
-              src={github}
-              alt="github"
-              className='w-1/2 h-1/2 object-contain'
-            />
-
-          </div>
-
-          
-
         </div>
+      </div>
+
+      <div className='mt-5'>
+             <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+             <p className='mt-2 text-secondary text-[14px]'>{description}</p> 
+      </div>
+
+      <div className='mt-4 flex flex-wrap gap-2'>
+        {tags.map((tag) => (
+          <p key={tag.name} className={`text-[14px]
+           ${tag.color}`} >
+            #{tag.name}
+            </p>
+        ))}
 
       </div>
 
