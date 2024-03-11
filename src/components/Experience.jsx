5bc7c92a763a176daab4bebfd9bbc8358800f225
pwaguzi <saisishaun4@gmail.com>
2024-03-11 10:42:23 +0300

    done with the work experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -15,7 +15,8 @@ const ExperienceCard = ({ experience }) => (
     date={experience.date}
     iconStyle={{ background: experience.iconBg }}
     icon={
-      <div>
+      <div className='flex justify-center items-center 
+      w-full h-full'>
         <img 
           src={experience.icon}
           alt={experience.company_name}
@@ -26,7 +27,26 @@ const ExperienceCard = ({ experience }) => (
   >
     <div>
       <h3 className='text-white text-[24px] font-bold'>{experience.title}</h3>
+      <p className='text-secondary text-[16px] font-semibold 
+      style={{ margin: 0 }}'>
+        {experience.company_name}
+
+      </p>
     </div>
+
+    <ul className='mt-5 list-disc ml-5 space-y-2'>
+      {experience.points.map((point, index) => (
+        <li 
+          key={`experience-point-${index}`}
+          className='text-white-100 text-[14px] pl-1 -tracking-wider'
+        >
+          {point}
+
+        </li>
+      ))}
+
+    </ul>
+
   </VerticalTimelineElement>
 )
 
