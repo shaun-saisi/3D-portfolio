141d11bf7bb4f5a7513f99b25b5c614ac3bdd269
pwaguzi <saisishaun4@gmail.com>
2024-03-13 17:03:00 +0300

    connected the contact form to email js

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,12 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+
+//
+//template_o613559
+
+//
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -33,8 +39,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_delkx8f',
+        'template_o613559',
         {
           from_name: form.name,
           to_name: "Anunda Shaun",
@@ -42,7 +48,7 @@ const Contact = () => {
           to_email: "anundashaun@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        'WVGJgQ5sUMeldjvyz'
       )
       .then(
         () => {
